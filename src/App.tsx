3199d44314f70d2b227719c9edfaae3bdd759f34
ifahimDigiden <iftekhar.fahim@digidenit.com>
2025-07-12 21:58:57 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 ">
+      <h1 className="text-2xl mb-4">Intern Reaction Project</h1>
+      <Button type="primary">Test AntD Button</Button>
+    </div>
+  );
+}
+
+export default App;
