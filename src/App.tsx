36d9d5a7f3066360fb5fe66725179bfebc761093
masum-DigidenIt <ataharmasum@digidenit.com>
2025-07-13 16:57:12 +0600

    text changed

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 ">
       <h1 className="text-2xl mb-4">Intern Reaction Project</h1>
       <Button type="primary">Test AntD Button</Button>
-      <h1>test</h1>
+      <h1>In Progress</h1>
     </div>
   );
 }
