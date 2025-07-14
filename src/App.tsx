f0dd129194d3bdf5cbea82f9ea3259fef441878c
Tasfia-DigidenIT <tasfiatabassum@digidenit.com>
2025-07-14 10:59:29 +0600

    conflict resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { Button } from "antd";
+import Dashboard from "./layout/Dashboard";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 ">
-      <h1 className="text-2xl mb-4">Intern Reaction Project</h1>
+    <div className="flex flex-col justify-center items-center bg-gray-100 min-h-screen">
+      <h1 className="mb-4 text-2xl">Intern Reaction Project</h1>
       <Button type="primary">Test AntD Button</Button>
+
+      <Dashboard></Dashboard>
+
+      <h1>In Progress</h1>
+      <h2 className="text-cyan-600"> text Changed</h2>
+
     </div>
   );
 }
